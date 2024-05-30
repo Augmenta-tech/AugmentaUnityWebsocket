ba6051cc423b08657a7bd6ae2d88583a9c1cdc8a
Ben Kuper <bkuperberg@gmail.com>
2024-05-30 12:44:36 +0200

    add streaming options and update register

--- Runtime/AugmentaClient.cs
+++ Runtime/AugmentaClient.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System;
 using UnityEngine.Events;
 using WebSocketSharp;
+using OSCQuery;
 
 namespace Augmenta
 {
@@ -45,16 +46,28 @@ namespace Augmenta
         float lastMessageTime;
 
 
-        [Header("Setup")]
+        [Header("Spawn")]
         public GameObject objectPrefab;
         public GameObject zonePrefab;
 
-
         [Header("Connection")]
+        public string clientName = "Unity";
+        public string clientID;
         [SerializeField] string _ipAddress = "127.0.0.1";
         [SerializeField] int _port = 6060;
         public bool connected = false;
         public bool receivingData = false;
+
+        [Header("Streaming Options")]
+        public int downSample = 1;
+        public bool streamClouds = true;
+        public bool streamClusters = true;
+        public List<string> tags;
+
+        int _lastDownSample = 1;
+        bool _lastStreamClouds = true;
+        bool _lastStreamClusters = true;
+        List<string> _lastTags;
 
 
         [Header("Events")]
@@ -100,6 +113,7 @@ namespace Augmenta
             {
                 connected = true;
                 Debug.Log("Connection " + "ws://" + ipAddress + ":" + port + " opened !");
+                sendRegister();
             };
 
             websocket.OnError += (sender, e) =>
@@ -115,7 +129,6 @@ namespace Augmenta
 
             websocket.OnMessage += (sender, e) =>
             {
-                Debug.Log("Receive message : " + e.IsText);
                 connected = true;
                 lastMessageTime = lastUpdateTime;
 
@@ -125,6 +138,31 @@ namespace Augmenta
 
             };
 
+        }
+
+
+        public void sendRegister()
+        {
+            JSONObject o = JSONObject.Create();
+            o.AddField("id", clientID);
+            o.AddField("name", clientName);
+            JSONObject options = JSONObject.Create();
+            options.AddField("downSample", downSample);
+            options.AddField("streamClouds", streamClouds);
+            options.AddField("streamClusters", streamClusters);
+            JSONObject tagsO = JSONObject.Create();
+            foreach(var t in tags) tagsO.Add(t);
+            options.AddField("tags", tagsO);
+            JSONObject coords = JSONObject.Create();
+            coords.AddField("axis", "y_up_left");
+            coords.AddField("origin", "bottom_left");
+            options.AddField("coordinateMapping", coords);
+            o.AddField("options", options);
+
+            JSONObject ro = JSONObject.Create();
+            ro.AddField("register", o);
+
+            websocket.Send(ro.ToString());
         }
 
         void processMessage(MessageEventArgs e)
@@ -161,6 +199,32 @@ namespace Augmenta
         void Update()
         {
             if (websocket == null) init();
+
+
+            if(connected)
+            {
+                bool tagsChanged = false;
+                if (tags.Count != _lastTags.Count) tagsChanged = true;
+                else
+                {
+                    for (int i = 0; i < tags.Count; i++)
+                    {
+                        if (tags[i] != _lastTags[i])
+                        {
+                            tagsChanged = true;
+                            break;
+                        }
+                    }
+                }
+                if(streamClouds != _lastStreamClouds || streamClusters != _lastStreamClusters || downSample != _lastDownSample || tagsChanged)
+                {
+                    sendRegister();
+                    _lastStreamClouds = streamClouds;
+                    _lastStreamClusters = streamClusters;
+                    _lastTags = new List<string>(tags);
+                }
+            }
+
 
 #if !UNITY_WEBGL || UNITY_EDITOR
             if ((!connected || !websocket.IsAlive) && Time.time - lastConnectTime > 1)
