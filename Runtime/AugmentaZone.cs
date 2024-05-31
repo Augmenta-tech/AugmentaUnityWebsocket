ccb0377de818d4523ea18b2c2803c4d4b9e9817d
Ben Kuper <bkuperberg@gmail.com>
2024-05-31 18:16:10 +0200

    add client live update

--- Runtime/AugmentaZone.cs
+++ Runtime/AugmentaZone.cs
@@ -27,7 +27,33 @@ namespace Augmenta
         private void OnDrawGizmos()
         {
             Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.lossyScale);
-            Gizmos.color = Color.red;
+            Gizmos.color = Color.cyan;
+
+            if(nativeZone.shape != null)
+            {
+                switch (nativeZone.shape.shapeType)
+                {
+                    case Shape<Vector3>.ShapeType.Box:
+                        BoxShape<Vector3> box = nativeZone.shape as BoxShape<Vector3>;
+                        Gizmos.DrawWireCube(box.size/2, box.size);
+                        break;
+                    case Shape<Vector3>.ShapeType.Sphere:
+                        SphereShape<Vector3> sphere = nativeZone.shape as SphereShape<Vector3>;
+                        Gizmos.DrawWireSphere(Vector3.zero, sphere.radius);
+                        break;
+                    case Shape<Vector3>.ShapeType.Cylinder:
+                        CylinderShape<Vector3> cylinder = nativeZone.shape as CylinderShape<Vector3>;
+                        Vector3 halfSize = new Vector3(cylinder.radius, cylinder.height / 2, cylinder.radius);
+                        Gizmos.DrawWireCube(halfSize, halfSize*2);
+                        break;
+                    case Shape<Vector3>.ShapeType.Cone:
+                        ConeShape<Vector3> cone = nativeZone.shape as ConeShape<Vector3>;
+                        Gizmos.DrawWireSphere(Vector3.zero, cone.radius);
+                        break;
+                    case Shape<Vector3>.ShapeType.Mesh:
+                        break;
+                }
+            }
             //Gizmos.DrawWireCube(Vector3.zero, nativeZone.size);
         }
     }
